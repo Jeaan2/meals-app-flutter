561fca5597c81767ace08827c74a4a8760fefd06
Jeaan2 <jeaan3@gmail.com>
2022-04-20 21:26:41 -0300

    entire code

---
+++ lib/screens/favorite_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import '../models/meal.dart';
+import '../components/meal_item.dart';
+
+class FavoriteScreen extends StatelessWidget {
+  final List<Meal> favoriteMeals;
+
+  const FavoriteScreen(this.favoriteMeals);
+
+  @override
+  Widget build(BuildContext context) {
+    if (favoriteMeals.isEmpty) {
+      return const Center(
+        child: Text('Não tem nada aqui :('),
+      );
+    } else {
+      return ListView.builder(
+        itemCount: favoriteMeals.length,
+        itemBuilder: (context, index) {
+          return MealItem(favoriteMeals[index]);
+        },
+      );
+    }
+  }
+}
---
+++ lib/screens/tabs_screen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+import 'categories_screen.dart';
+import 'favorite_screen.dart';
+import '../components/main_drawer.dart';
+import '../models/meal.dart';
+
+class TabsScreen extends StatefulWidget {
+  final List<Meal> favoriteMeals;
+
+  const TabsScreen(this.favoriteMeals);
+
+  @override
+  State<TabsScreen> createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  int _selectedScreenIndex = 0;
+
+  final List<String> _titles = [
+    'Lista de Categorias',
+    'Meus Favoritos',
+  ];
+
+  List<Widget> _screens = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _screens = [
+      CategoriesScreen(),
+      FavoriteScreen(widget.favoriteMeals),
+    ];
+  }
+
+  _selectScreen(int index) {
+    setState(() {
+      _selectedScreenIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_titles[_selectedScreenIndex]),
+      ),
+      body: _screens[_selectedScreenIndex],
+      drawer: MainDrawer(),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectScreen,
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Colors.yellow,
+        currentIndex: _selectedScreenIndex,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            label: 'Categorias',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: 'Favoritos',
+          ),
+        ],
+      ),
+    );
+  }
+}
